838cadf5ed2f4df823e11a76a248691fc0a7885e
Bartek Witkowski <bartek@tibe.no>
2024-03-05 14:32:25 +0100

    refactor _vite.php

--- templates/parts/_vite.php
+++ templates/parts/_vite.php
@@ -1,7 +1,9 @@
-<?php if(isset($_ENV['ENVIRONMENT']) && $_ENV['ENVIRONMENT'] === 'dev') : ?>
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <script type="module" src="http://localhost:5173/resources/js/app.js"></script>
-    <script type="module" src="http://localhost:5173/resources/scss/app.scss"></script>
+<?php $isDev = isset($_ENV['ENVIRONMENT']) && $_ENV['ENVIRONMENT'] === 'dev' && isset($_ENV['VITE_PORT']) ?>
+
+<?php if($isDev) : ?>  
+    <script type="module" src="http://localhost:<?php echo $_ENV['VITE_PORT'] ?>/@vite/client"></script>
+    <script type="module" src="http://localhost:<?php echo  $_ENV['VITE_PORT'] ?>/resources/js/app.js"></script>
+    <script type="module" src="http://localhost:<?php echo  $_ENV['VITE_PORT'] ?>/resources/scss/app.scss"></script>
 <?php else: ?>
     <script type="module" src="/resources/js/app.js"></script>
     <link rel="stylesheet" href="/resources/css/app.css">
